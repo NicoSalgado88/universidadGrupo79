80a7c4658931594994e9ee35776aefb782da8911
CaroReale <caro.reale2212@gmail.com>
2023-09-21 17:14:22 -0300

    .
    .

--- src/universidadgrupo79/UniversidadGrupo79.java
+++ src/universidadgrupo79/UniversidadGrupo79.java
@@ -13,52 +13,52 @@ import universidadgrupo79.entidades.Materia;
 public class UniversidadGrupo79 {
 
 
-    public static void main(String[] args) {
-        Alumnos a=new Alumnos(5, 12345678, "Lope", "Anti", LocalDate.of(1993, Month.MARCH, 9), true);
-        
-//        AlumnoData data=new AlumnoData();
-//        data.guardarAlumno(a);
-//        data.modificarAlumno(a);
-//        data.eliminarAlumno(5);
-//        System.out.println(a.toString());
-
-//        Alumnos a=data.buscarAlumnoPorDni(12345678);
-//        System.out.println(a.toString());
-//        for (Alumnos alumno : data.listarAlumnos()) {
-//            System.out.println(alumno.toString());
-//        }
-        Materia m=new Materia(1,"Matematica", 3, true);
-
-//        MateriaData data=new MateriaData();
-//        data.modificarMateria(m);
-//        data.eliminarMateria(1);
-//        for (Materia materia : data.listarMaterias()) {
-//            System.out.println(materia.toString());
-//        }
-//        Inscripcion insc=new Inscripcion(a, m, 8);
-        InscripcionData data=new InscripcionData();
-//        data.guardarInscripcion(insc);
-//        data.borrarInscripcion(5, 1);
-//        System.out.println(data.buscarMateria(1).toString());
-//        data.guardarMateria(m);
-//        data.actualizarNota(5, 1, 7);
-//        for (Inscripcion insc : data.obtenerInscripciones()) {
-//            System.out.println(insc.toString());
-//        }
-//        for (Inscripcion insc : data.obtenerInscripcionesPorAlumno(6)) {
-//            System.out.println(insc.toString());
-//        }
-
-//        for (Materia mat : data.obtenerMateriasCursadas(5)) {
-//            System.out.println(mat.toString());
-//        }
-//        for (Materia mat : data.obtenerMateriasNOCursadas(5)) {
-//            System.out.println(mat.toString());
-//        }
-
-//        for (Alumnos alu : data.obtenerAlumnosPorMateria(2)) {
-//            System.out.println(alu.toString());
-//        }
-    }
-    
+//    public static void main(String[] args) {
+//        Alumnos a=new Alumnos(5, 12345678, "Lope", "Anti", LocalDate.of(1993, Month.MARCH, 9), true);
+//        
+////        AlumnoData data=new AlumnoData();
+////        data.guardarAlumno(a);
+////        data.modificarAlumno(a);
+////        data.eliminarAlumno(5);
+////        System.out.println(a.toString());
+//
+////        Alumnos a=data.buscarAlumnoPorDni(12345678);
+////        System.out.println(a.toString());
+////        for (Alumnos alumno : data.listarAlumnos()) {
+////            System.out.println(alumno.toString());
+////        }
+//        Materia m=new Materia(1,"Matematica", 3, true);
+//
+////        MateriaData data=new MateriaData();
+////        data.modificarMateria(m);
+////        data.eliminarMateria(1);
+////        for (Materia materia : data.listarMaterias()) {
+////            System.out.println(materia.toString());
+////        }
+////        Inscripcion insc=new Inscripcion(a, m, 8);
+//        InscripcionData data=new InscripcionData();
+////        data.guardarInscripcion(insc);
+////        data.borrarInscripcion(5, 1);
+////        System.out.println(data.buscarMateria(1).toString());
+////        data.guardarMateria(m);
+////        data.actualizarNota(5, 1, 7);
+////        for (Inscripcion insc : data.obtenerInscripciones()) {
+////            System.out.println(insc.toString());
+////        }
+////        for (Inscripcion insc : data.obtenerInscripcionesPorAlumno(6)) {
+////            System.out.println(insc.toString());
+////        }
+//
+////        for (Materia mat : data.obtenerMateriasCursadas(5)) {
+////            System.out.println(mat.toString());
+////        }
+////        for (Materia mat : data.obtenerMateriasNOCursadas(5)) {
+////            System.out.println(mat.toString());
+////        }
+//
+////        for (Alumnos alu : data.obtenerAlumnosPorMateria(2)) {
+////            System.out.println(alu.toString());
+////        }
+//    }
+//    
 }
